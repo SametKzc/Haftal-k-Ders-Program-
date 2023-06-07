70d9d1884285039e9963148f4a7ef87a330aa871
Samet <kuzucusamet01@gmail.com>
2023-06-07 19:30:24 +0300

    Add project files.

---
+++ Haftalık Ders Programı/DersprogramıOluştur.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Haftalık_Ders_Programı
+{
+    partial class DersprogramıOluştur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Derslikler_geri = new System.Windows.Forms.Button();
+            this.lblogretimElemanı = new System.Windows.Forms.Label();
+            this.lblders = new System.Windows.Forms.Label();
+            this.lblDerslik = new System.Windows.Forms.Label();
+            this.lblsaat = new System.Windows.Forms.Label();
+            this.lblgün = new System.Windows.Forms.Label();
+            this.cbxeleman = new System.Windows.Forms.ComboBox();
+            this.cbxders = new System.Windows.Forms.ComboBox();
+            this.cbxderslik = new System.Windows.Forms.ComboBox();
+            this.cbxsaat = new System.Windows.Forms.ComboBox();
+            this.cbxgun = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxogrenimturu = new System.Windows.Forms.ComboBox();
+            this.lblöğrenimtürü = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbxsınıf = new System.Windows.Forms.ComboBox();
+            this.lblsınıf = new System.Windows.Forms.Label();
+            this.Dgwsonuc = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgwsonuc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Derslikler_geri
+            // 
+            this.btn_Derslikler_geri.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_Derslikler_geri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btn_Derslikler_geri.Location = new System.Drawing.Point(12, 12);
+            this.btn_Derslikler_geri.Name = "btn_Derslikler_geri";
+            this.btn_Derslikler_geri.Size = new System.Drawing.Size(100, 49);
+            this.btn_Derslikler_geri.TabIndex = 9;
+            this.btn_Derslikler_geri.Text = "Geri";
+            this.btn_Derslikler_geri.UseVisualStyleBackColor = false;
+            // 
+            // lblogretimElemanı
+            // 
+            this.lblogretimElemanı.AutoSize = true;
+            this.lblogretimElemanı.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblogretimElemanı.Location = new System.Drawing.Point(8, 159);
+            this.lblogretimElemanı.Name = "lblogretimElemanı";
+            this.lblogretimElemanı.Size = new System.Drawing.Size(139, 20);
+            this.lblogretimElemanı.TabIndex = 10;
+            this.lblogretimElemanı.Text = "Öğretim Elemanı ";
+            // 
+            // lblders
+            // 
+            this.lblders.AutoSize = true;
+            this.lblders.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblders.Location = new System.Drawing.Point(8, 191);
+            this.lblders.Name = "lblders";
+            this.lblders.Size = new System.Drawing.Size(51, 20);
+            this.lblders.TabIndex = 11;
+            this.lblders.Text = "Ders ";
+            // 
+            // lblDerslik
+            // 
+            this.lblDerslik.AutoSize = true;
+            this.lblDerslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblDerslik.Location = new System.Drawing.Point(8, 224);
+            this.lblDerslik.Name = "lblDerslik";
+            this.lblDerslik.Size = new System.Drawing.Size(72, 20);
+            this.lblDerslik.TabIndex = 12;
+            this.lblDerslik.Text = "Derslik  ";
+            // 
+            // lblsaat
+            // 
+            this.lblsaat.AutoSize = true;
+            this.lblsaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblsaat.Location = new System.Drawing.Point(8, 256);
+            this.lblsaat.Name = "lblsaat";
+            this.lblsaat.Size = new System.Drawing.Size(48, 20);
+            this.lblsaat.TabIndex = 13;
+            this.lblsaat.Text = "Saat ";
+            // 
+            // lblgün
+            // 
+            this.lblgün.AutoSize = true;
+            this.lblgün.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblgün.Location = new System.Drawing.Point(8, 288);
+            this.lblgün.Name = "lblgün";
+            this.lblgün.Size = new System.Drawing.Size(45, 20);
+            this.lblgün.TabIndex = 14;
+            this.lblgün.Text = "Gün ";
+            // 
+            // cbxeleman
+            // 
+            this.cbxeleman.FormattingEnabled = true;
+            this.cbxeleman.Location = new System.Drawing.Point(168, 158);
+            this.cbxeleman.Name = "cbxeleman";
+            this.cbxeleman.Size = new System.Drawing.Size(249, 21);
+            this.cbxeleman.TabIndex = 15;
+            // 
+            // cbxders
+            // 
+            this.cbxders.FormattingEnabled = true;
+            this.cbxders.Location = new System.Drawing.Point(168, 190);
+            this.cbxders.Name = "cbxders";
+            this.cbxders.Size = new System.Drawing.Size(249, 21);
+            this.cbxders.TabIndex = 16;
+            // 
+            // cbxderslik
+            // 
+            this.cbxderslik.FormattingEnabled = true;
+            this.cbxderslik.Location = new System.Drawing.Point(168, 223);
+            this.cbxderslik.Name = "cbxderslik";
+            this.cbxderslik.Size = new System.Drawing.Size(249, 21);
+            this.cbxderslik.TabIndex = 17;
+            // 
+            // cbxsaat
+            // 
+            this.cbxsaat.FormattingEnabled = true;
+            this.cbxsaat.Location = new System.Drawing.Point(168, 255);
+            this.cbxsaat.Name = "cbxsaat";
+            this.cbxsaat.Size = new System.Drawing.Size(249, 21);
+            this.cbxsaat.TabIndex = 18;
+            // 
+            // cbxgun
+            // 
+            this.cbxgun.FormattingEnabled = true;
+            this.cbxgun.Location = new System.Drawing.Point(168, 282);
+            this.cbxgun.Name = "cbxgun";
+            this.cbxgun.Size = new System.Drawing.Size(249, 21);
+            this.cbxgun.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(148, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(148, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(148, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label4.Location = new System.Drawing.Point(148, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label5.Location = new System.Drawing.Point(148, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = ":";
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnkaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnkaydet.Location = new System.Drawing.Point(205, 324);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(100, 49);
+            this.btnkaydet.TabIndex = 25;
+            this.btnkaydet.Text = "Kaydet";
+            this.btnkaydet.UseVisualStyleBackColor = false;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnOlustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnOlustur.Location = new System.Drawing.Point(1034, 659);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(100, 49);
+            this.btnOlustur.TabIndex = 27;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label6.Location = new System.Drawing.Point(148, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 20);
+            this.label6.TabIndex = 30;
+            this.label6.Text = ":";
+            // 
+            // cbxogrenimturu
+            // 
+            this.cbxogrenimturu.FormattingEnabled = true;
+            this.cbxogrenimturu.Location = new System.Drawing.Point(168, 131);
+            this.cbxogrenimturu.Name = "cbxogrenimturu";
+            this.cbxogrenimturu.Size = new System.Drawing.Size(249, 21);
+            this.cbxogrenimturu.TabIndex = 29;
+            // 
+            // lblöğrenimtürü
+            // 
+            this.lblöğrenimtürü.AutoSize = true;
+            this.lblöğrenimtürü.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblöğrenimtürü.Location = new System.Drawing.Point(8, 132);
+            this.lblöğrenimtürü.Name = "lblöğrenimtürü";
+            this.lblöğrenimtürü.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblöğrenimtürü.Size = new System.Drawing.Size(107, 20);
+            this.lblöğrenimtürü.TabIndex = 28;
+            this.lblöğrenimtürü.Text = "Öğrenim türü";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label8.Location = new System.Drawing.Point(148, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(14, 20);
+            this.label8.TabIndex = 33;
+            this.label8.Text = ":";
+            // 
+            // cbxsınıf
+            // 
+            this.cbxsınıf.FormattingEnabled = true;
+            this.cbxsınıf.Location = new System.Drawing.Point(168, 104);
+            this.cbxsınıf.Name = "cbxsınıf";
+            this.cbxsınıf.Size = new System.Drawing.Size(249, 21);
+            this.cbxsınıf.TabIndex = 32;
+            this.cbxsınıf.SelectedIndexChanged += new System.EventHandler(this.cbxsınıf_SelectedIndexChanged);
+            // 
+            // lblsınıf
+            // 
+            this.lblsınıf.AutoSize = true;
+            this.lblsınıf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.lblsınıf.Location = new System.Drawing.Point(8, 105);
+            this.lblsınıf.Name = "lblsınıf";
+            this.lblsınıf.Size = new System.Drawing.Size(42, 20);
+            this.lblsınıf.TabIndex = 31;
+            this.lblsınıf.Text = "Sınıf";
+            // 
+            // Dgwsonuc
+            // 
+            this.Dgwsonuc.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.Dgwsonuc.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.Dgwsonuc.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Dgwsonuc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgwsonuc.Location = new System.Drawing.Point(468, 70);
+            this.Dgwsonuc.Name = "Dgwsonuc";
+            this.Dgwsonuc.Size = new System.Drawing.Size(652, 568);
+            this.Dgwsonuc.TabIndex = 26;
+            this.Dgwsonuc.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Dgwsonuc_CellContentClick);
+            // 
+            // DersprogramıOluştur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 720);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cbxsınıf);
+            this.Controls.Add(this.lblsınıf);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbxogrenimturu);
+            this.Controls.Add(this.lblöğrenimtürü);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.Dgwsonuc);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxgun);
+            this.Controls.Add(this.cbxsaat);
+            this.Controls.Add(this.cbxderslik);
+            this.Controls.Add(this.cbxders);
+            this.Controls.Add(this.cbxeleman);
+            this.Controls.Add(this.lblgün);
+            this.Controls.Add(this.lblsaat);
+            this.Controls.Add(this.lblDerslik);
+            this.Controls.Add(this.lblders);
+            this.Controls.Add(this.lblogretimElemanı);
+            this.Controls.Add(this.btn_Derslikler_geri);
+            this.Name = "DersprogramıOluştur";
+            this.Text = "DersprogramıOluştur";
+            this.Load += new System.EventHandler(this.DersprogramıOluştur_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgwsonuc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Derslikler_geri;
+        private System.Windows.Forms.Label lblogretimElemanı;
+        private System.Windows.Forms.Label lblders;
+        private System.Windows.Forms.Label lblDerslik;
+        private System.Windows.Forms.Label lblsaat;
+        private System.Windows.Forms.Label lblgün;
+        private System.Windows.Forms.ComboBox cbxeleman;
+        private System.Windows.Forms.ComboBox cbxders;
+        private System.Windows.Forms.ComboBox cbxderslik;
+        private System.Windows.Forms.ComboBox cbxsaat;
+        private System.Windows.Forms.ComboBox cbxgun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbxogrenimturu;
+        private System.Windows.Forms.Label lblöğrenimtürü;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbxsınıf;
+        private System.Windows.Forms.Label lblsınıf;
+        private System.Windows.Forms.DataGridView Dgwsonuc;
+    }
+}
---
+++ Haftalık Ders Programı/DersprogramıOluştur.cs
@@ -0,0 +1,199 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Haftalık_Ders_Programı
+{
+    public partial class DersprogramıOluştur : Form
+    {
+        MySqlConnection conn = new MySqlConnection("Server=localhost;Database=haftalık_ders_programı;Uid=root;Pwd='';");
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        DataTable dt;
+        public DersprogramıOluştur()
+        {
+            InitializeComponent();
+        }
+        void VeriYenile()
+        {
+            dt = new DataTable();
+            conn.Open();
+            adapter = new MySqlDataAdapter("SELECT *FROM dersprogramı", conn);
+            adapter.Fill(dt);
+            cbxders.DataSource = dt;
+            conn.Close();
+        }
+        void derscek()
+        {
+
+                MySqlCommand komut = new MySqlCommand("SELECT * FROM dersler WHERE yarıyıl = @Yarıyıl", conn);
+                komut.Parameters.AddWithValue("@Yarıyıl", cbxsınıf.Text);
+                MySqlDataReader dr;
+                conn.Open();
+                dr = komut.ExecuteReader();
+                cbxders.Items.Clear(); // Daha önceki dersleri temizle
+
+                while (dr.Read())
+                {
+                    cbxders.Items.Add("( " + dr["ders_kodu"] + " ) " + dr["ders_adı"] + " ( " + dr["ders_türü"] + ")");
+                }
+                conn.Close();
+        }
+        void elemanscek()
+        {
+            MySqlCommand komut = new MySqlCommand("SELECT * FROM ogretimuyeleri", conn);
+            MySqlDataReader dr;
+            conn.Open();
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                cbxeleman.Items.Add(dr["Ad"] + " " + dr["Soyad"]);
+            }
+            conn.Close();
+        }
+        void derslikcek()
+        {
+            MySqlCommand komut = new MySqlCommand("SELECT * FROM derslikler", conn);
+
+            MySqlDataReader dr;
+            conn.Open();
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                
+                cbxderslik.Items.Add(dr["derslik_kodu"] + " " + dr["derslik_türü"]);
+            }
+            conn.Close();
+        }
+        void comboboxvericek()
+        {
+                cbxgun.Items.Add("PAZARTESİ");
+                cbxgun.Items.Add("SALI");
+                cbxgun.Items.Add("ÇARŞAMBA");
+                cbxgun.Items.Add("PERŞEMBE");
+                cbxgun.Items.Add("CUMA");
+                cbxsaat.Items.Add("17.30-18.20");
+                cbxsaat.Items.Add("18.25-19.15");
+                cbxsaat.Items.Add("19.20-20.10");
+                cbxsaat.Items.Add("20.15-21.05");
+                cbxsaat.Items.Add("21.10-22.00");
+                cbxsaat.Items.Add("22.05-22.55");
+                cbxsaat.Items.Add("23.00-23.50");
+                cbxsaat.Items.Add("23.55-00.45");
+                cbxsaat.Items.Add("00.50-01.40");
+                cbxsınıf.Items.Add("1. Yarıyıl");
+                cbxsınıf.Items.Add("2. Yarıyıl");
+                cbxsınıf.Items.Add("3. Yarıyıl");
+                cbxsınıf.Items.Add("4. Yarıyıl");
+                cbxsınıf.Items.Add("5. Yarıyıl");
+                cbxsınıf.Items.Add("6. Yarıyıl");
+                cbxsınıf.Items.Add("7. Yarıyıl");
+                cbxsınıf.Items.Add("8. Yarıyıl");
+                cbxogrenimturu.Items.Add("1.Öğretim");
+                cbxogrenimturu.Items.Add("2.Öğretim");
+        }
+        void VerileriCek()
+        {
+            conn.Open();
+
+            string sql = "SELECT * FROM dersprogramı  WHERE Gün = @gun ORDER BY Saat ASC";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            // cbxgun.SelectedItem değerini kontrol et
+            if (cbxgun.SelectedItem != null)
+            {
+                cmd.Parameters.AddWithValue("@gun", cbxgun.SelectedItem.ToString());
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@gun", DBNull.Value);
+            }
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            adapter.Fill(dt);
+
+            Dgwsonuc.Columns.Clear();
+
+            // Pazartesi sütunu ekle
+            Dgwsonuc.Columns.Add("Pazartesi", "Pazartesi");
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Dgwsonuc.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                // Verileri pazartesi sütununun altına ekle
+                Dgwsonuc.Rows.Add( row["Saat"]+"\n"+ row["Ders"]+" \n "+row["OgretimElemani"]+" \n " + row["Derslik"]);
+            }
+
+            conn.Close();
+        }
+
+        private void DersprogramıOluştur_Load(object sender, EventArgs e)
+        {
+
+            comboboxvericek();
+            derslikcek();
+            elemanscek();
+            
+        }
+
+        private void Dgwsonuc_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            string sql ="INSERT INTO dersprogramı(Ders,Derslik,OgretimElemani,Gün,Saat,Sınıf,OgretimTuru)"+
+                "VALUES(@ders,@derslik,@ogretimelemani,@gun,@saat,@sınıf,@ogretimturu)";
+            cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ders", cbxders.Text);
+            cmd.Parameters.AddWithValue("@derslik", cbxderslik.Text);
+            cmd.Parameters.AddWithValue("@ogretimelemani",cbxeleman.Text);
+            cmd.Parameters.AddWithValue("@gun", cbxgun.Text);
+            cmd.Parameters.AddWithValue("@saat", cbxsaat.Text);
+            cmd.Parameters.AddWithValue("@sınıf", cbxsınıf.Text);
+            cmd.Parameters.AddWithValue("@ogretimturu", cbxogrenimturu.Text);
+
+            if ((cbxders.Text != null) && (cbxderslik.Text != null) && (cbxeleman.Text != null) && (cbxgun.Text != null) && (cbxogrenimturu.Text != null) && (cbxsaat.Text != null) && (cbxsınıf != null))
+            {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+                        MessageBox.Show("Kayıt Eklendi");
+
+            }
+            else
+                MessageBox.Show("Boş alan mevcut!!");
+
+
+            VerileriCek();
+
+
+
+
+
+
+
+
+
+            
+        }
+
+        private void cbxsınıf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            derscek();
+        }
+
+    }
+}
